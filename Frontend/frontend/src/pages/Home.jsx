6206b8f95cc8e2a8727d74ef88d0e7d532e57676
commander-maker <nimeshdhalpegama@gmail.com>
2025-11-12 20:01:57 +0530

    push before the pipeline(Jenkins)

--- Frontend/frontend/src/pages/Home.jsx
+++ Frontend/frontend/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../css/home.css';
+import homeImage from '../assets/home1.jpg';
 
 function Home() {
   return (
-    <div>
-      <header>
-        <h1>Welcome to the Home Page</h1>
-        <nav>
-          <a href="/">Logout</a>
-        </nav>
-      </header>
+    <div  className='home-page'>
+      <Navbar />
+      <div className="home-banner" style={{ backgroundImage: `url(${homeImage})` }}>
+        <div className="home-banner-text">
+          <h1>Find It Local</h1>
+          <p>May be It is Near You</p>
+        </div>
+      </div>
       <main>
         <p>This is a simple home page after login/signup.</p>
       </main>
+      <Footer />
     </div>
   );
 }
